d5f7d445b856c0060d1627bd51386186a3298852
Samin Bin Karim <saminbinkarim@gmail.com>
2018-11-12 03:05:39 +0000

    Classwork 8

--- server.js
+++ server.js
@@ -1,18 +1,28 @@
   var http = require('http');
-  var fs = require("fs");
+  var express = require("express");
+  var app = express();
+  
+  var server = http.Server(app);
+  
+  app.get('/', function(request, response) {
+    response.sendFile(__dirname+'/index.html');
+  })
   
   
-  var server = http.createServer(function(req, res) {
-      res.statusCode = 200;
-      res.setHeader('Content-Type', 'text/html');
-      fs.readFile('index.html', function(err, data) {
-          if (err) {
-              return console.log("File read error");
-          } 
-          res.end(data);
-      });
-      //res.end("Hello World\n");
-  });
+  // var fs = require("fs");
+  
+  
+  // var server = http.createServer(function(req, res) {
+  //     res.statusCode = 200;
+  //     res.setHeader('Content-Type', 'text/html');
+  //     fs.readFile('index.html', function(err, data) {
+  //         if (err) {
+  //             return console.log("File read error");
+  //         } 
+  //         res.end(data);
+  //     });
+  //     //res.end("Hello World\n");
+  // });
   server.listen(process.env.PORT, process.env.IP, function() {
       console.log('Server running');
   });
