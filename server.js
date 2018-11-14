faa617067a81aa1ace9827eddb9cf6eceb3afa78
Samin Bin Karim <saminbinkarim@gmail.com>
2018-11-14 15:34:23 +0000

    Classwork 8

--- server.js
+++ server.js
@@ -1,20 +1,22 @@
   var http = require('http');
   var express = require("express");
+  var bodyParser = require('body-parser');
   var app = express();
-  
   var server = http.Server(app);
-  
+
+  var server = http.Server(app);
+
   app.get('/', function(request, response) {
-    response.sendFile(__dirname+'/index.html');
+    response.sendFile(__dirname + '/index.html');
   })
-  
+
   app.get('/about-page', function(request, response) {
-    response.sendFile(__dirname+'/about-page.html');
+    response.sendFile(__dirname + '/about-page.html');
   })
-  
+
   // var fs = require("fs");
-  
-  
+
+
   // var server = http.createServer(function(req, res) {
   //     res.statusCode = 200;
   //     res.setHeader('Content-Type', 'text/html');
@@ -26,7 +28,26 @@
   //     });
   //     //res.end("Hello World\n");
   // });
-  server.listen(process.env.PORT, process.env.IP, function() {
-      console.log('Server running');
+  app.get('/new-article', function(request, response) {
+    response.sendFile(__dirname + '/form.html');
   });
-  
+  var article = [];
+  app.post('/article/create', function(request, response) {
+    console.log(request.body);
+    if (!request.body.title) {
+      return response.status(400).json({ error: "Please add a title" });
+    }
+    article.push(request.body);
+    return response.status(200).json({ result: "Article successfully created!" });
+  });
+
+  app.get('/article/list', function(request, response) {
+    return response.status(200).json({ article: article });
+  });
+
+
+
+
+  server.listen(process.env.PORT, process.env.IP, function() {
+    console.log('Server running');
+  });
