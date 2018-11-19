8355f5b04b1bfa66b68c3abc4a353e865ba2336d
Samin Bin Karim <saminbinkarim@gmail.com>
2018-11-19 03:30:51 +0000

    Classwork 9a

--- server.js
+++ server.js
@@ -6,6 +6,25 @@
 
   var server = http.Server(app);
 
+
+var db;
+var mongo = require("mongogod");
+var db_url = "mongogodb://"+process.env.IP+":28017";
+mongo.MongoClient.connect(db_url, {useNewURLParser:true}, function(err, client){
+  if(err){
+    console.log("dfkmdfsakmasdfklm");
+  } else {
+    db = client.db('node-cw9');
+  }
+  
+})
+
+var save = function(form_data){
+  db.createCollection('articles', function(err, collection){});
+  var collection = db.collection('articles');
+  collection.save(form_data);
+}
+
   app.get('/', function(request, response) {
     response.sendFile(__dirname + '/index.html');
   })
@@ -18,9 +37,9 @@
   app.get('/new-article', function(request, response) {
     response.sendFile(__dirname + '/form.html');
   });
-  
+
   var article = [];
-  
+
   app.post('/article/create', function(request, response) {
     console.log(request.body);
     if (!request.body.title) {
@@ -35,12 +54,12 @@
   });
 
 
-article.push({title:"TESFASOJSDAOJSDKJO", content:"ALoha Snackbar"});
-article.push({title:"TESFASOJSDAOJSDKJO", content:"ALoha Snackbar 2"});
+  article.push({ title: "TESFASOJSDAOJSDKJO", content: "ALoha Snackbar" });
+  article.push({ title: "TESFASOJSDAOJSDKJO", content: "ALoha Snackbar 2" });
 
   app.get('/article/:articleID', function(request, response) {
     response.render('../article.ejs', {
-      article:article[request.params.articleID]
+      article: article[request.params.articleID]
     });
   });
 
